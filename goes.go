105b7be7b7ace82b03513e8aa3e5cc66ac4a4487
Kevin Paul Herbert <kph@platinasystems.com>
2020-08-07 13:31:35 -0400

    goes-boot: Add install command

--- goes.go
+++ goes.go
@@ -44,6 +44,7 @@ import (
 	"github.com/platinasystems/goes/cmd/ifcmd"
 	"github.com/platinasystems/goes/cmd/iminfo"
 	"github.com/platinasystems/goes/cmd/insmod"
+	"github.com/platinasystems/goes/cmd/install"
 	"github.com/platinasystems/goes/cmd/iocmd"
 	"github.com/platinasystems/goes/cmd/ip"
 	"github.com/platinasystems/goes/cmd/kexec"
@@ -124,6 +125,7 @@ var Goes = &goes.Goes{
 		"hset":    hset.Command{},
 		"if":      &ifcmd.Command{},
 		"insmod":  insmod.Command{},
+		"install": install.Command{},
 		"io":      iocmd.Command{},
 		"ip":      ip.Goes,
 		"kexec":   &kexec.Command{},
